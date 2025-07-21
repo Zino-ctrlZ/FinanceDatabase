109b84b7f07cd66f32a2edc0946d186b348db066
Zino-ctrlZ <onokpisekome@gmail.com>
2025-07-20 22:00:41 -0500

    create order

--- dbase/DataAPI/Alpaca.py
+++ dbase/DataAPI/Alpaca.py
@@ -24,6 +24,8 @@ def collect_params(args_dict, exclude: list[str] = []):
     params = {}
     for k, v in args_dict.items():
         if k not in exclude and v is not None:
+            if isinstance(v, list):
+                params[k] = ','.join(v)
             params[k] = v
     return params
 
@@ -55,25 +57,26 @@ def get_account(trading_client: TradingClient):
     return trading_client.get_account()
 
 
-"""
-Get option chain from Alpaca
-ref: https://docs.alpaca.markets/reference/optionchain
-base url: https://data.alpaca.markets/v1beta1/options/snapshots/{symbol}
-params: 
-- symbol: str
-- type: str
-- expiration_date: str
-- expiration_date_gte: str
-- expiration_date_lte: str
-- strike_price_gte: str
-- strike_price_lte: str
-- updated_since: str
-- limit: int
-- page_token: str
 
-returns : {next_page_token: str, snapshots: dict} | None
-"""
 def get_option_chain(symbol: str, type: str, **kwargs): 
+    """
+    Get option chain from Alpaca
+    ref: https://docs.alpaca.markets/reference/optionchain
+    base url: https://data.alpaca.markets/v1beta1/options/snapshots/{symbol}
+    params: 
+    - symbol: str
+    - type: str
+    - expiration_date: str
+    - expiration_date_gte: str
+    - expiration_date_lte: str
+    - strike_price_gte: str
+    - strike_price_lte: str
+    - updated_since: str
+    - limit: int
+    - page_token: str
+
+    returns : {next_page_token: str, snapshots: dict} | None
+    """
     params = collect_params({**kwargs, 'type': type}, exclude=['symbol'])
     url = f'https://data.alpaca.markets/v1beta1/options/snapshots/{symbol}'
     url = add_query_params(url, params)
@@ -84,26 +87,27 @@ def get_option_chain(symbol: str, type: str, **kwargs):
         return None
     return response.json()
 
-"""
-Get all option chains from Alpaca
-ref: https://docs.alpaca.markets/reference/optionchain
-base url: https://data.alpaca.markets/v1beta1/options/snapshots/{symbol}
-params: 
-- symbol: str
-- type: str
-- expiration_date: str
-- expiration_date_gte: str
-- expiration_date_lte: str
-- strike_price_gte: str
-- strike_price_lte: str
-- updated_since: str
-- limit: int
-- page_token: str
 
-returns : dict | None
-"""
 
 def get_option_chain_all(symbol: str, type: str, **kwargs): 
+    """
+    Get all option chains from Alpaca
+    ref: https://docs.alpaca.markets/reference/optionchain
+    base url: https://data.alpaca.markets/v1beta1/options/snapshots/{symbol}
+    params: 
+    - symbol: str
+    - type: str
+    - expiration_date: str
+    - expiration_date_gte: str
+    - expiration_date_lte: str
+    - strike_price_gte: str
+    - strike_price_lte: str
+    - updated_since: str
+    - limit: int
+    - page_token: str
+
+    returns : dict | None
+    """
     res = get_option_chain(symbol, type, **kwargs)
     snapshots = {}
     if res is None: 
@@ -117,26 +121,27 @@ def get_option_chain_all(symbol: str, type: str, **kwargs):
     return snapshots
 
 
-"""
-Get option contracts from Alpaca
-ref: https://docs.alpaca.markets/reference/get-options-contracts
-params: 
-- underlying_symbols: str
-- type: str
-- expiration_date: str
-- expiration_date_gte: str
-- expiration_date_lte: str
-- strike_price_gte: str
-- strike_price_lte: str
-- updated_since: str
-- limit: int
-- page_token: str
 
-returns : {option_contracts: list[dict], next_page_token: str | None} | None
-"""
 def get_option_contracts(underlying_symbols: str, type: str, **kwargs): 
+    """
+    Get option contracts from Alpaca
+    ref: https://docs.alpaca.markets/reference/get-options-contracts
+    params: 
+    - underlying_symbols: str
+    - type: str
+    - expiration_date: str
+    - expiration_date_gte: str
+    - expiration_date_lte: str
+    - strike_price_gte: str
+    - strike_price_lte: str
+    - updated_since: str
+    - limit: int
+    - page_token: str
+
+    returns : {option_contracts: list[dict], next_page_token: str | None} | None
+    """
     params = collect_params({**kwargs, 'type': type, 'underlying_symbols': underlying_symbols})
-    url = get_base_url(paper=True) + '/options/contracts'
+    url = get_base_url() + '/options/contracts'
     url = add_query_params(url, params)
     response = requests.get(url, headers=get_headers())
     if response.status_code != 200:
@@ -145,24 +150,25 @@ def get_option_contracts(underlying_symbols: str, type: str, **kwargs):
         return None
     return response.json()
 
-"""
-Get all option contracts from Alpaca
-ref: https://docs.alpaca.markets/reference/get-options-contracts
-params: 
-- underlying_symbols: str
-- type: str
-- expiration_date: str
-- expiration_date_gte: str
-- expiration_date_lte: str
-- strike_price_gte: str
-- strike_price_lte: str
-- updated_since: str
-- limit: int
-- page_token: str
 
-returns : list[dict] | None
-"""
 def get_option_contracts_all(underlying_symbols: str, type: str, **kwargs): 
+    """
+    Get all option contracts from Alpaca
+    ref: https://docs.alpaca.markets/reference/get-options-contracts
+    params: 
+    - underlying_symbols: str
+    - type: str
+    - expiration_date: str
+    - expiration_date_gte: str
+    - expiration_date_lte: str
+    - strike_price_gte: str
+    - strike_price_lte: str
+    - updated_since: str
+    - limit: int
+    - page_token: str
+
+    returns : list[dict] | None
+    """
     res = get_option_contracts(underlying_symbols, type, **kwargs)
     option_contracts = []
     if res is None: 
@@ -174,3 +180,95 @@ def get_option_contracts_all(underlying_symbols: str, type: str, **kwargs):
             if res is not None:
                 option_contracts.extend(res.get('option_contracts', []))
     return option_contracts
+
+
+
+def get_option_by_id_or_symbol(symbol_or_id: str): 
+    """
+    Get option by id or symbol from Alpaca
+    ref: https://docs.alpaca.markets/reference/get-option-contract-symbol_or_id
+    params: 
+    - symbol_or_id: str
+
+    returns : dict | None
+    """
+    url = get_base_url() + f'/options/contracts/{symbol_or_id}'
+    response = requests.get(url, headers=get_headers())
+    if response.status_code != 200:
+        logger.error(f'Error getting option by id or symbol: {response.status_code} {response.text}')
+        print(f'Error: {response.text} {response.status_code}')
+        raise Exception(f'Error getting option contracts: {response.status_code} {response.text}')
+    return response.json()
+
+
+
+def get_orders(status: str, **kwargs): 
+    """
+    Get all orders
+    ref: https://docs.alpaca.markets/reference/getallorders-1
+    params: 
+    - status: str 'closed' | 'open' | 'all'
+    - limit: int
+    - after: str timestamp (YYYY-MM-DDTHH:MM:SSZ)
+    - until: str timestamp (YYYY-MM-DDTHH:MM:SSZ)
+    - direction: str 'asc' | 'desc'
+    - nested: bool
+    - symbols: list[str]
+    - asset_class: str 'us_option' | 'us_equity' | 'crypto' | 'all'
+
+    returns : list[dict] | None
+    """
+    params = collect_params({**kwargs, 'status': status})
+    url = get_base_url() + '/orders'
+    url = add_query_params(url, params)
+    response = requests.get(url, headers=get_headers())
+    if response.status_code != 200:
+        logger.error(f'Error getting orders: {response.status_code} {response.text}')
+        print(f'Error: {response.text} {response.status_code}')
+        raise Exception(f'Error getting orders: {response.status_code} {response.text}')
+    return response.json()
+
+
+def create_order(symbol: str, **kwargs): 
+    """
+    Create order
+    ref: https://docs.alpaca.markets/reference/postorder
+    params: 
+    - symbol: str
+    - qty: int
+    - side: str
+    - type: str
+    - time_in_force: str
+    - limit_price: float
+    - stop_price: float
+    - client_order_id: str
+
+    returns : dict | None
+    """
+    payload = collect_params({**kwargs, 'symbol': symbol}, exclude=['qty', 'side', 'type', 'time_in_force', 'limit_price', 'stop_price', 'client_order_id'])
+    url = get_base_url() + '/orders'
+    response = requests.post(url, headers=get_headers(), json=payload)
+    if response.status_code != 200:
+        logger.error(f'Error creating order: {response.status_code} {response.text}')
+        print(f'Error: {response.text} {response.status_code}')
+        raise Exception(f'Error creating order: {response.status_code} {response.text}')
+    return response.json()
+    
+
+def delete_order(order_id: str): 
+    """
+    Delete order
+    ref: https://docs.alpaca.markets/reference/deleteallorders-1
+    param:
+    - order_id: str
+
+    returns : dict | None
+    """
+    url = get_base_url() + f'/orders/{order_id}'
+    response = requests.delete(url, headers=get_headers())
+    if response.status_code != 200:
+        logger.error(f'Error deleting order: {response.status_code} {response.text}')
+        print(f'Error: {response.text} {response.status_code}')
+        raise Exception(f'Error deleting order: {response.status_code} {response.text}')
+    return response.json()
+
